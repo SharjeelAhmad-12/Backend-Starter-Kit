0b1b7ec24df5af3d8f257858303d3b07cdbb7828
Sharjeel Ahmad <156166566+SharjeelAhmad-12@users.noreply.github.com>
2025-07-14 22:16:14 +0500

    refactor: Send email  add error handling

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,30 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+   console.log(process.env.EMAIL_USER)
+   console.log(process.env.EMAIL_PASS)
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  });
+    const mailOptions = {
+      from: `"Your App Name" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw new Error('Failed to send email');
+  }
 };
 
 module.exports = sendEmail;
