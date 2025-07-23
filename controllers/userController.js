5f124363750bbc457248eff5cba049dd830d08f3
Iman-Fatima0 <iman.f8094@gmail.com>
2025-07-23 15:30:23 +0500

    create():UserController + feat(): GetSearchedUsers

---
+++ controllers/userController.js
@@ -0,0 +1,61 @@
+const User= require("../models/User");
+
+const GetSearchedUsers=async(req,res)=>
+{
+    try{
+        const {search,role,isVerified,page=1,limit=10}=req.query;
+        const regex= new RegExp(search, 'i');
+        if(search)
+        {
+            query.$or=[
+                {name:{$regex:regex}},
+                {email:{$regex:regex}}
+            ];
+        }
+
+        if(role)
+        {
+            query.role=role;
+        }
+
+        if(isVerified=='true' || isVerified=='false')
+        {
+            query.isVerified=isVerified==="true";
+        }
+
+        const totalusers= await User.countDocuments(query);
+        const totalpages=Math.ceil(totalusers/limit);
+        const currentPage=Number(page);
+
+        const users= await User.find(query)
+            .skip((currentPage-1)*limit)
+            .limit(Number(limit))
+            .select("-password -otp")
+            .sort({createdAt:-1});
+
+            return res.status(200).json({
+                success:true,
+                message:"Users fetched successfully",
+                data:users,
+                totalUsers:totalusers,
+                totalPages:totalpages,
+                currentPage
+            });
+
+
+    }
+    catch(error)
+    {
+        console.error("Admin User Fetch Error:", error);
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error",
+            error:error.message
+        });
+
+    }
+}
+
+module.exports = {
+    GetSearchedUsers            
+};
