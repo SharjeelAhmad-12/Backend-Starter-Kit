9bc741106d4ce5d6451f63efaa769f397495ef68
Iman-Fatima0 <iman.f8094@gmail.com>
2025-07-23 15:43:24 +0500

    fix():wrong middleware name eported + add(): routes to server.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router= express.Router();
 const { GetSearchedUsers } = require("../controllers/userController");
-const {isAuthenticated,authorizeRoles} = require("../middlewares/authMiddleware");
+const {authMiddleware,authorizeRoles} = require("../middlewares/authMiddleware");
 
-router.get("/search-users", isAuthenticated, authorizeRoles("admin"), GetSearchedUsers);
+router.get("/search-users", authMiddleware, authorizeRoles("admin"), GetSearchedUsers);
 
 module.exports = router;
