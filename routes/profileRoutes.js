1d7f9fa2aa16ccc411369d394cf424a914bdcfb6
Fizza-Ashfaq <fizza0849@gmail.com>
2025-07-25 15:47:01 +0500

    Added DI in cloudinary routes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -8,7 +8,6 @@ const verifyToken=require("../middlewares/verifyToken")
 const validateRequest = require("../middlewares/validateRequest");
 const upload = require("../middlewares/uploadMiddleware");
 const { updateProfileSchema } = require("../validations/profileValidation");
-
 const router = express.Router();
 
 router.get("/", verifyToken, getProfile);
--- utils/cloudinaryUpload.js
+++ utils/cloudinaryUpload.js
@@ -1,21 +1,23 @@
-const cloudinary = require("../config/cloudinary");
 const streamifier = require("streamifier");
 
-const streamUpload = (buffer, options = {}) =>
-  new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        folder: "profiles",
-        resource_type: "image",
-        transformation: [{ width: 500, crop: "limit" }],
-        ...options,
-      },
-      (error, result) => {
-        if (result) resolve(result);
-        else reject(error);
-      }
-    );
-    streamifier.createReadStream(buffer).pipe(stream);
-  });
+const streamUpload = (cloudinary) => {
+  return async (buffer, options = {}) => {
+    return new Promise((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        {
+          folder: "profiles",
+          resource_type: "image",
+          transformation: [{ width: 500, crop: "limit" }],
+          ...options,
+        },
+        (error, result) => {
+          if (result) resolve(result);
+          else reject(error);
+        }
+      );
+      streamifier.createReadStream(buffer).pipe(stream);
+    });
+  };
+};
 
 module.exports = streamUpload;
